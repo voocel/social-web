f8441fa900b71e7a58d20988731af80d53f82d64
voocel <voocel@gmail.com>
2023-10-04 23:37:16 +0800

    feat: add message api

---
+++ src/api/message.js
@@ -0,0 +1,11 @@
+import http from '@/utils/http'
+import qs from 'qs'
+
+const message = {
+  // 获取历史消息
+  getGroups(params) {
+    return http.get('/message/list', qs.stringify(params))
+  }
+}
+
+export default message
