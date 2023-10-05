3e596a86e09410db0993326083f931734664f9df
voocel <voocel@gmail.com>
2023-10-05 17:52:33 +0800

    feat: support indexdb

---
+++ src/store/idb.config.js
@@ -0,0 +1,28 @@
+const config = {
+  dbName: 'social',
+  ver: 1,
+  debug: true,
+  objectStores: [
+    {
+      objectStoreName: 'msg',
+      index: [ // 索引 ， unique 是否可以重复
+        // { name: 'uid', unique: false }
+      ]
+    }
+  ],
+  objects: { // 初始化数据
+    // msg: [
+    //   {
+    //     sender_id: 1,
+    //     receiver_id: 2,
+    //     nickname: 'xxx',
+    //     avatar: 'xxx',
+    //     concent: 'xxx',
+    //     content_type: 1,
+    //     timeline: 20231010
+    //   },
+    // ]
+  }
+}
+
+export default config
